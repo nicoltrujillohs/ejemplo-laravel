2364cab1c6557f9946c19479529315c075a33389
Nicol Trujillo <nicol.trujillo0212@alumnos.udg.mx>
2023-09-05 19:38:29 -0600

    Formulario

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>FORMULARIO</h1>
+    <form action="">
+        <label for="Correo">Correo</label><br>
+        <input type="email" name="Correo" /><br>
+        <label for="Comentario">Comentarios</label><br>
+        <input type="text" name="Comentario" /><br>
+
+        <button>Enviar</butto>
+    </form>
+</body>
+</html>
